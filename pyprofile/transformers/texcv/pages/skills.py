08d47f21f962979658d8ed1ad5e069ef0bdf0b89
windet01 <theo.windebank@bbc.co.uk>
2018-11-17 13:18:50 +0000

    add negative vspace to skills

--- pyprofile/transformers/texcv/pages/skills.py
+++ pyprofile/transformers/texcv/pages/skills.py
@@ -6,6 +6,6 @@ from pyprofile.transformers.texcv import templates
 def generate(profile):
     skills = [skill['name'] for skill in profile.get('skills')]
     cvitem = " ~~·~~ ".join(skills)
-    cventry = templates.cventry(cvitems=[cvitem])
+    cventry = templates.cventry(cvitems=[cvitem], vspace="-1em")
     cvsection = templates.cvsection('Skills', [cventry])
     return inspect.cleandoc(cvsection)
